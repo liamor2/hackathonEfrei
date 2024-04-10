1a5f359dfedcafdd87c5a7cfe9ae23e6d6f4028a
SVergnol <samuel.vergnol@lucem-conseil.com>
2024-04-10 10:05:16 +0200

    default values

--- init.sql
+++ init.sql
@@ -48,3 +48,96 @@ CREATE TABLE Avis(
     FOREIGN KEY(idVin) REFERENCES Vins(idVin),
     FOREIGN KEY(idUser) REFERENCES Users(idUser)
 );
+
+INSERT INTO
+    Vins (
+        nomVin,
+        Domaine,
+        Region,
+        Pays,
+        tauxAlcool,
+        petillant,
+        volume,
+        bio,
+        couleur,
+        urlImage,
+        cépages,
+        millésime,
+        description
+    )
+VALUES
+    (
+        'Château Lafite Rothschild',
+        'Domaine Barons de Rothschild (Lafite)',
+        'Pauillac',
+        'France',
+        12.5,
+        FALSE,
+        750.00,
+        FALSE,
+        'Red',
+        'https://example.com/image1.jpg',
+        'Cabernet Sauvignon, Merlot',
+        2018,
+        'A classic Pauillac with rich tannins, deep blackcurrant flavors, and a long finish.'
+    ),
+    (
+        'Dom Pérignon',
+        'Moët & Chandon',
+        'Champagne',
+        'France',
+        12.0,
+        TRUE,
+        750.00,
+        FALSE,
+        'White',
+        'https://example.com/image2.jpg',
+        'Chardonnay, Pinot Noir',
+        2010,
+        'A prestigious champagne that offers complexity, silkiness, and a celebrated history.'
+    ),
+    (
+        'Opus One',
+        'Opus One Winery',
+        'Napa Valley',
+        'USA',
+        14.0,
+        FALSE,
+        750.00,
+        FALSE,
+        'Red',
+        'https://example.com/image3.jpg',
+        'Cabernet Sauvignon, Merlot, Cabernet Franc, Petit Verdot, Malbec',
+        2016,
+        'A harmonious blend offering a mix of cassis, cedar, and floral notes with a balanced acidity.'
+    ),
+    (
+        'Penfolds Grange',
+        'Penfolds',
+        'South Australia',
+        'Australia',
+        14.5,
+        FALSE,
+        750.00,
+        FALSE,
+        'Red',
+        'https://example.com/image4.jpg',
+        'Shiraz',
+        2015,
+        'A powerful expression of Australian Shiraz, with rich fruit flavors and a robust structure.'
+    ),
+    (
+        'Sassicaia',
+        'Tenuta San Guido',
+        'Tuscany',
+        'Italy',
+        13.5,
+        FALSE,
+        750.00,
+        FALSE,
+        'Red',
+        'https://example.com/image5.jpg',
+        'Cabernet Sauvignon, Cabernet Franc',
+        2017,
+        'A Super Tuscan renowned for its exquisite elegance, balance, and aging potential.'
+    );
